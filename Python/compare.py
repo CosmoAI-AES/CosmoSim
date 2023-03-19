742aa97f7ef7ea336c7ad8161063d6835c341cdd
Hans Georg Schaathun <georg@schaathun.net>
2023-03-19 11:59:28 +0100

    Compare individual files.  The Sampled Lens does not work at all.

--- Python/compare.py
+++ Python/compare.py
@@ -29,11 +29,16 @@ if __name__ == "__main__":
           description = 'Compare images for debugging purposes',
           epilog = '')
 
+    parser.add_argument('-f', '--files', action='store_true', 
+            help="Compare individual files")
     parser.add_argument('dir1')
     parser.add_argument('dir2')
     args = parser.parse_args()
 
-    for fn in os.listdir(args.dir1):
+    if args.files:
+        comparefiles(args.dir1,args.dir2)
+    else:
+      for fn in os.listdir(args.dir1):
         f1 = os.path.join(args.dir1,fn)
         if not os.path.isfile(f1): 
             print( "Not a file: ", f1 )
---
+++ sampled.sh
@@ -0,0 +1,16 @@
+dir=Test/20230319/
+python3 Python/compare.py --files $dir/image-s001.png  $dir/image-ss01.png
+python3 Python/compare.py --files $dir/image-s002.png  $dir/image-ss02.png
+python3 Python/compare.py --files $dir/image-s003.png  $dir/image-ss03.png
+python3 Python/compare.py --files $dir/image-s004.png  $dir/image-ss04.png
+python3 Python/compare.py --files $dir/image-s005.png  $dir/image-ss05.png
+python3 Python/compare.py --files $dir/image-ts11.png  $dir/image-ss11.png
+python3 Python/compare.py --files $dir/image-ts12.png  $dir/image-ss12.png
+python3 Python/compare.py --files $dir/image-ts13.png  $dir/image-ss13.png
+python3 Python/compare.py --files $dir/image-ts14.png  $dir/image-ss14.png
+python3 Python/compare.py --files $dir/image-ts15.png  $dir/image-ss15.png
+
+
+
+
+
