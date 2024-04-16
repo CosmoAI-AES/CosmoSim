93815218ca6132b8a95896a94b6eb0321f2ebffa
Hans Georg Schaathun <georg@schaathun.net>
2024-04-16 21:20:13 +0200

    montage of SIS sampling test

--- Test/sampling/samplingtest.sh
+++ Test/sampling/samplingtest.sh
@@ -10,11 +10,22 @@ dir1=SampledSIS
 dir2=SIS
 diffdir=diff
 
-mkdir -p $dir1 $dir2 $diffdir
+rm -rf Actual
+mkdir -p $dir1 $dir2 $diffdir Actual
 
 fn=../spheres.csv
 
-python3 $pdir/datagen.py $opt --config "Sampled Raytrace SIS" --directory="$dir1" --csvfile $fn 
+python3 $pdir/datagen.py $opt --config "Sampled Raytrace SIS" --directory="$dir1" --csvfile $fn   --actual
+
+mv SampledSIS/actual* Actual/
 python3 $pdir/datagen.py $opt --config "Raytrace SIS" --directory="$dir2" --csvfile $fn 
 python3 $pdir/compare.py --diff $diffdir $dir1 $dir2
 
+CONVERT=convert
+mkdir -p montage
+
+for f in diff/*
+do
+  ff=`basename $f`
+  $CONVERT \( Actual/actual-$ff SIS/$ff +append \) \( SampledSIS/$ff diff/$ff +append \) -append montage/$ff
+done
