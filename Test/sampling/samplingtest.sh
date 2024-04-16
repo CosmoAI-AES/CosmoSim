60c08bd07f8763d9ef993147b6a3ada8805b5ca4
Hans Georg Schaathun <georg@schaathun.net>
2024-04-16 11:37:40 +0200

    Updated the SIS samplingtest

--- Test/sampling/samplingtest.sh
+++ Test/sampling/samplingtest.sh
@@ -4,16 +4,17 @@
 
 opt=$*
 
-pdir=../../Python
+pdir=../../CosmoSimPy
 dir1=SampledSIS
-dir2=RouletteSIS
+# dir2=RouletteSIS
+dir2=SIS
 diffdir=diff
 
 mkdir -p $dir1 $dir2 $diffdir
 
 fn=../spheres.csv
 
-python3 $pdir/datagen.py $opt -L ss --directory="$dir1" --csvfile $fn 
-python3 $pdir/datagen.py $opt -L sr --directory="$dir2" --csvfile $fn 
+python3 $pdir/datagen.py $opt --config "Sampled Raytrace SIS" --directory="$dir1" --csvfile $fn 
+python3 $pdir/datagen.py $opt --config "Raytrace SIS" --directory="$dir2" --csvfile $fn 
 python3 $pdir/compare.py --diff $diffdir $dir1 $dir2
 
