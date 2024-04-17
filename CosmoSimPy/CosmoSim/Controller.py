a04023cae44f430a08e425070fcdb817f6ed2495
Hans Georg Schaathun <georg@schaathun.net>
2024-04-17 11:31:48 +0200

    Added sliders, but they are not connected to the model

--- CosmoSimPy/CosmoSim/Controller.py
+++ CosmoSimPy/CosmoSim/Controller.py
@@ -13,6 +13,46 @@ import math
 from CosmoSim import modelValues, sourceValues
 
 # Classes
+class FloatSlider:
+    """
+    A slider for integer values with label.
+    """
+    def __init__(self,root,text,row=1,fromval=0,toval=100,var=None,
+            resolution=0.01, default=0):
+        """
+        Set up the slider with the following parameters:
+
+        :param root: parent widget
+        :param text: label text
+        :param row: row number in the parent grid
+        :param fromval: lower bound on the range
+        :param toval: upper bound on the range
+        :param var: variable object to use for the value 
+                    (IntVar instanceby default)
+        :param resolution: resolution of the variable (default 1)
+        """
+        if var == None:
+            self.var = DoubleVar()
+        else:
+            self.var = var
+        self.var.set( default )
+        self.label = ttk.Label( root, text=text,
+                style="Std.TLabel" )
+        self.slider = Scale( root, length=250, variable=self.var,
+                resolution=resolution,
+                digits=3,
+                orient=HORIZONTAL,
+                from_=fromval, to=toval )
+        self.label.grid(row=row,column=0,sticky=E)
+        self.slider.grid(row=row,column=1)
+        # self.val = ttk.Label( root, textvariable=self.var )
+        # self.val.grid(row=row,column=2)
+    def get(self):
+        "Get the value of the slider."
+        return self.var.get()
+    def set(self,v): 
+        "Set the value of the slider."
+        return self.var.get(v)
 class IntSlider:
     """
     A slider for integer values with label.
@@ -192,11 +232,19 @@ class LensPane(ttk.Frame):
         self.einsteinSlider = IntSlider( self,
             text="Einstein Radius", row=2,
             default=20 )
+        self.ratioSlider = FloatSlider( self,
+            text="Ellipticity", row=3,
+            fromval=0,
+            toval=1,
+            default=1 )
+        self.orientationSlider = IntSlider( self, 
+                toval=360,
+                text="Lens Orientation", row=4, default=45 )
         self.chiSlider = IntSlider( self,
-            text="Distance Ratio (chi)", row=3,
+            text="Distance Ratio (chi)", row=5,
             default=50 )
         self.ntermsSlider = IntSlider( self, 
-            text="Number of Terms (Roulettes only)", row=4,
+            text="Number of Terms (Roulettes only)", row=6,
             toval=50,
             default=16 )
         self.einsteinSlider.var.trace_add( "write", self.push ) 
