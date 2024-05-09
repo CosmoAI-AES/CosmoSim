11e8e44fcefa2e78ce56d4349726f3b723f76501
Hans Georg Schaathun <georg@schaathun.net>
2024-05-09 10:58:32 +0200

    Bugfix in SIS::criticalXi

--- src/simlib/SIS.cpp
+++ src/simlib/SIS.cpp
@@ -18,10 +18,8 @@ double SIS::psiYfunction( double x, double y ) {
    return einsteinR*y/s ;
 }
 
-double SIE::criticalXi( double phi ) {
+double SIS::criticalXi( double phi ) {
    double c = cos(phi-orientation*PI/180) ;
    double s = sin(phi-orientation*PI/180) ;
-   double xicrit = sqrt(f)*einsteinR ;
-   xicrit /= 2 ;
-   return xicrit ;
+   return einsteinR/2 ;
 }
