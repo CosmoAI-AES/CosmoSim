f9afcb922f260924504566aa9e4649f301f625e1
Hans Georg Schaathun <georg@schaathun.net>
2024-07-02 21:46:20 +0200

    drafted roulettestatistics.py

--- CosmoSimPy/roulettestatistics.py
+++ CosmoSimPy/roulettestatistics.py
@@ -13,10 +13,31 @@ if __name__ == "__main__":
           description = 'Roulette Statistics',
           epilog = '')
 
+    parser.add_argument("-o",'--outfile',required=False)
     parser.add_argument('fn')
     args = parser.parse_args()
 
     dataset = pd.read_csv( args.fn )
     cols = dataset.columns
     ramp = RouletteAmplitudes( cols )
+
+    with open(outfile,"w") as out:
+        out.write( "\\begin{tabular}{|rr|rr|rr|}\n"
+        out.write( "\\hline\n" )
+        out.write( " $m$ & $s$ & $\\bar\\alpha_m^s$ & $\\mathsf{stdev}(\\alpha_m^s)$" )
+        out.write( "     & $\\bar\\beta_m^s$ & $\\mathsf{stdev}(\\beta_m^s)$ \\\\\n" )
+        out.write( "\\hline\n" )
+        out.write( "\\hline\n" )
+        for m in range(ramp.getNterms()+1):
+            for s in range((m+1)%2,m+2,2):
+                 avg = dataset[f"alpha[{s}][{m}]"].mean()
+                 sd = dataset[f"alpha[{s}][{m}]"].std()
+                 out.write( f"  ${m}$ & ${s}$ & ")
+                 out.write( f"${avg}$ & ${sd}$ & ")
+                 avg = dataset[f"beta[{s}][{m}]"].mean()
+                 sd = dataset[f"beta[{s}][{m}]"].std()
+                 out.write( f"${avg}$ & ${sd}$ \\\\\n ")
+ 
+        out.write( "\\hline\n" )
+        out.close()
 
