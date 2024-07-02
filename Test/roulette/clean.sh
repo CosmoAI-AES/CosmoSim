e8c27b816f0564e0ca337795778f9388b78fdfe7
Hans Georg Schaathun <georg@schaathun.net>
2024-07-02 09:53:03 +0200

    Drafted roulette comparison

---
+++ Test/roulette/clean.sh
@@ -0,0 +1,2 @@
+
+rm -rf rousie srousie raysie sraysie actual-* diff-* montage-*
---
+++ Test/roulette/sie.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+CONVERT=convert
+
+opt=$*
+
+pdir=../../CosmoSimPy/
+fn=sie.csv
+
+mkdir Raytrace actual montage
+python3 $pdir/datagen.py $opt --config "Raytrace SIS" --directory="Raytrace" --csvfile $fn  --actual -R
+mv Raytrace/actual*png actual
+
+for m in 3 5 15
+do
+   mkdir Roulette$m  diff$m
+   python3 $pdir/datagen.py $opt --config "Roulette SIS" --directory="Roulette$m" --csvfile $fn  --nterms $m -R
+   python3 $pdir/compare.py --diff diff$m Raytrace Roulette$m
+done
+
+
+for f in Raytrace/*
+do
+  ff=`basename $f`
+  $CONVERT \( actual/actual-$ff Raytrace/$ff +append \) \
+     \( Roulette3/$ff diff3/$ff +append \) \
+     \( Roulette5/$ff diff5/$ff +append \) \
+     \( Roulette15/$ff diff15/$ff +append \) \
+     -append montage/$ff
+done
