eae0e32788868090ae5e0f5d10a731d12067dc33
Hans Georg Schaathun <georg@schaathun.net>
2024-07-05 10:43:22 +0200

    using --xireference in roulette statistics test

--- Test/roulettestatistics/test.sh
+++ Test/roulettestatistics/test.sh
@@ -7,7 +7,7 @@ mkdir -p Original Roulette diff montage
 # python3 ./datasetgen.py
 
 python3 $pyd/datagen.py -Z 600 --csvfile dataset.csv --outfile roulette.csv \
-   --model Roulette --xireference -D Original --actual  -R --nterms 9
+   --model Roulette --lens SIS --xireference -D Original --actual  -R --nterms 9
 
 python3 $pyd/roulettegen.py -n 10 -Z 600 --csvfile roulette.csv --xireference -D Roulette -R --nterms 9
 
