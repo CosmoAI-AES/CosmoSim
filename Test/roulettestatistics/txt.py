1f0db30608e962c31a9099220329f24d23708351
Hans Georg Schaathun <georg@schaathun.net>
2024-07-05 13:37:10 +0200

    Drafted code to include parametrs in TeX report

---
+++ Test/roulettestatistics/txt.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+
+src = pd.read_csv( "roulette.csv" )
+
+df = src[["filename","chi","einsteinR","x","y","sigma","sigma2","theta"]].head(10)
+
+for row in df:
+    fn = row["filename"].split(".")[0] + ".txt"
+    with open(fn,"w") as f:
+        f.write(row.to_string())
+        f.close()
