c512e7d2a4e5758a34d5092022f305278df9ce0d
Hans Georg Schaathun <georg@schaathun.net>
2024-07-05 12:16:45 +0200

    script to tweak amplitudes

---
+++ Test/roulettestatistics/tweak.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+
+df = pd.read_csv( "roulette.csv" )
+df["beta[5][2]"] = df["beta[5][2]"] * 10
+df.to_csv( "roulette-beta.csv" )
+df["beta[5][2]"] = 0
+df.to_csv( "roulette-beta0.csv" )
+
+df = pd.read_csv( "roulette.csv" )
+df["alpha[5][2]"] = df["alpha[5][2]"] * 10
+df.to_csv( "roulette-alpha.csv" )
+df["alpha[5][2]"] = 0
+df.to_csv( "roulette-alpha0.csv" )
+
+df = pd.read_csv( "roulette.csv" )
+lab = [ f"alpha[5][{s}]" for s in [ 2, 4, 6 ] ]
+df[lab] = 10*df[lab]
+df.to_csv( "roulette-alphas.csv" )
+blab = [ f"beta[5][{s}]" for s in [ 2, 4, 6 ] ]
+df[blab] = 10*df[blab]
+df.to_csv( "roulette-ab.csv" )
+df[lab+blab] = 0.01*df[lab+blab]
+df.to_csv( "roulette-min.csv" )
