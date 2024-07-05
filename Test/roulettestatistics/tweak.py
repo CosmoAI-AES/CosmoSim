b8871621f1b120554e1f9d6fc5365cd3d7ece670
Hans Georg Schaathun <georg@schaathun.net>
2024-07-05 13:04:09 +0200

    Complete draft of the tweak test

--- Test/roulettestatistics/tweak.py
+++ Test/roulettestatistics/tweak.py
@@ -4,18 +4,24 @@ import pandas as pd
 
 src = pd.read_csv( "roulette.csv" )
 
+print( "tweak base set")
 df = src.head(10)
+df.to_csv( "roulette-base.csv" )
+
+print( "tweak first set")
 df["beta[5][2]"] = df["beta[5][2]"] * 10
 df.to_csv( "roulette-beta.csv" )
 df["beta[5][2]"] = 0
 df.to_csv( "roulette-beta0.csv" )
 
+print( "tweak second set")
 df = src.head(10)
 df["alpha[5][2]"] = df["alpha[5][2]"] * 10
 df.to_csv( "roulette-alpha.csv" )
 df["alpha[5][2]"] = 0
 df.to_csv( "roulette-alpha0.csv" )
 
+print( "tweak third set")
 df = src.head(10)
 lab = [ f"alpha[5][{s}]" for s in [ 2, 4, 6 ] ]
 df[lab] = 10*df[lab]
@@ -25,3 +31,5 @@ df[blab] = 10*df[blab]
 df.to_csv( "roulette-ab.csv" )
 df[lab+blab] = 0.01*df[lab+blab]
 df.to_csv( "roulette-min.csv" )
+
+print( "done tweaking datasets")
--- Test/roulettestatistics/tweak.sh
+++ Test/roulettestatistics/tweak.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-./tweak.sh
+./tweak.py
 
 pyd=../../CosmoSimPy
 
@@ -8,5 +8,6 @@ for i in roulette-*.csv
 do
    f=`basename $i .csv`
    mkdir $f
+   echo Generating images for $f
    python3 $pyd/roulettegen.py -Z 600 --csvfile $i --xireference -D $f -R --nterms 5
 done
