93ef40e8294881f790bfafc69c7e5b8a407b3beb
Hans Georg Schaathun <georg@schaathun.net>
2024-07-16 12:44:30 +0200

    Drafted the rotated derivatives for SIE

--- CosmoSimPy/derivatives.py
+++ CosmoSimPy/derivatives.py
@@ -13,12 +13,13 @@ import sys
 import os
 import time
 import argparse
+import queue
 
 import sympy
 from libamplitudes import *
-from sympy import symbols, sqrt, diff, sin, cos, asin, atan2, asinh
+from sympy import symbols, sqrt, diff, sin, cos, asin, atan2, asinh, binomial
 
-def mainworker(q,outq,maxm=6):
+def firstworker(q,outq,maxm=6):
     print ( os.getpid(),"working" )
     while True:
         i,j,f,x,y = q.get(True)   # Blocks until there is a job on the queue
@@ -26,35 +27,47 @@ def mainworker(q,outq,maxm=6):
            res = sympy.simplify( diff( f, y ) )
         else:
            res = sympy.simplify( diff( f, x ) )
-        if i < maxm:     # Submit jobs for next round
+        if i+j < maxm:     # Submit jobs for next round
             q.put( (i+1, j, res, x, y) ) 
             if i==0:
                q.put( (0, j+1, res, x, y) ) 
-        print( os.getpid(), i, j, res )
+        print( "I", os.getpid(), i, j, res )
         outq.put( (i,j,res) )
         q.task_done()     # Tell the queue that the job is complete
 
         # Note that new jobs are submitted to the queue before 
         # `q.task_done()` is called.  Hence the queue will not be
         # empty if the current job should spawn new ones. 
+    print ( os.getpid(),"returning" )
 
+def secondworker(q,outq,diff1,theta ):
+    print ( os.getpid(),"working" )
+    cont = True
+    while cont:
+      try:
+        m,n = q.get(False)   # does not block
+        res = sympy.simplify(sum( [
+                  binomial( m+n, i+j )*
+                  cos(theta)**(m-i+j)*
+                  sin(theta)**(n-j+i)*
+                  (-1)**i
+                  * diff1[(m-i+j,n-j+i)]
+                  for i in range(m+1) for j in range(n+1) ] ) )
+        print( "II", os.getpid(), m, n, res )
+        outq.put(res)
+      except queue.Empty:
+        print ( "II", os.getpid(), "completes" )
+        cont = False
 
-def main(lens="SIS",n=50,nproc=None,fn=None):
+    print ( os.getpid(),"returning" )
 
-    if nproc is None: nproc = n
-    print( f"Using {nproc} CPUs" )
-    
-    # The filename is generated from the number of amplitudes
-    if fn is None: fn = str(n) + '.txt'
-
-    start = time.time()
-
+def getDict(n=50,nproc=None):
     q = mp.JoinableQueue()  # Input queue.  It is joinable to control completion.
     outq = mp.Queue()       # Output queue
 
     # Get and store the initial case m+s=1
-    (a,b,x,y) = psiSIE()
-    dict = { (1,0) : a, (0,1) : b }
+    (a,b,x,y) = psiSIE0()
+    resDict = { (1,0) : a, (0,1) : b }
 
     # Submit first round of jobs
     q.put( (2,0,a,x,y) )
@@ -62,11 +75,41 @@ def main(lens="SIS",n=50,nproc=None,fn=None):
     q.put( (0,2,b,x,y) )
 
     # Create a pool of workers to process the input queue.
-    pool = mp.Pool(nproc, mainworker,(q,outq,n,))
+    with mp.Pool(nproc, firstworker,(q,outq,n,)) as pool: 
 
-    q.join()   # Block until the input jobs are completed
+       q.join()   # Block until the input jobs are completed
+       print( "I getDict() joined queue" )
 
-    print( "Time spent:", time.time() - start)
+    print( "I pool closed" )
+
+    while not outq.empty():
+        i,j,res = outq.get()
+        resDict[(i,j)] = res
+    print( "I getDict returns" )
+    return resDict
+
+def getDiff(n,nproc,diff1):
+    q = mp.Queue()          # Input queue
+    outq = mp.Queue()       # Output queue
+
+    psidiff = {}
+    theta = symbols("p",positive=True,real=True)
+    for i in range(n+1):
+       for j in range(n-i+1):
+           if i+j > 0: q.put((i,j))
+    pool = mp.Pool(nproc, secondworker,(q,outq,diff1,theta))
+    q.close()
+    pool.close()
+    pool.join()
+    print( "II pool closed" )
+
+    while not outq.empty():
+        i,j,res = outq.get()
+        psidiff[(i,j)] = res
+
+    print( "II getDiff returns" )
+    return psidiff
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate roulette amplitude formulæ for CosmoSim.')
@@ -74,11 +117,26 @@ if __name__ == "__main__":
                     help='Max m (number of terms)')
     parser.add_argument('nproc', type=int, nargs="?",
                     help='Number of processes.')
-    parser.add_argument('--lens', default="SIE",
-                    help='Lens model')
     parser.add_argument('--output', help='Output filename')
     parser.add_argument('--diff', default=False,action="store_true",
                     help='Simply differentiate psi')
     args = parser.parse_args()
 
-    main(lens=args.lens,n=args.n,nproc=args.nproc,fn=args.output)
+    n = args.n
+    if args.nproc is None: 
+        nproc = n
+    else:
+        nproc = args.nproc
+    print( f"Using {nproc} CPUs" )
+    
+    # The filename is generated from the number of amplitudes
+    if args.output is None:
+        fn = str(n) + '.txt'
+    else:
+        fn = args.fn
+
+    start = time.time()
+    diff1 = getDict(n,nproc)
+    diff2 = getDiff(n,nproc,diff1)
+
+    print( "Time spent:", time.time() - start)
