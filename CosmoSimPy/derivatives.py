30c5d38d92ccccfe2edb0443009ff1d6c395833b
Hans Georg Schaathun <georg@schaathun.net>
2024-07-16 09:09:42 +0200

    comments

--- CosmoSimPy/derivatives.py
+++ CosmoSimPy/derivatives.py
@@ -18,51 +18,53 @@ import sympy
 from libamplitudes import *
 from sympy import symbols, sqrt, diff, sin, cos, asin, atan2, asinh
 
-time.sleep(10)
-
-
 def mainworker(q,outq,maxm=6):
     print ( os.getpid(),"working" )
     while True:
-        i,j,f,x,y = q.get(True)
+        i,j,f,x,y = q.get(True)   # Blocks until there is a job on the queue
         if i == 0:
            res = sympy.simplify( diff( f, y ) )
         else:
            res = sympy.simplify( diff( f, x ) )
-        if i < maxm:
+        if i < maxm:     # Submit jobs for next round
             q.put( (i+1, j, res, x, y) ) 
             if i==0:
                q.put( (0, j+1, res, x, y) ) 
         print( os.getpid(), i, j, res )
         outq.put( (i,j,res) )
-        q.task_done()
+        q.task_done()     # Tell the queue that the job is complete
+
+        # Note that new jobs are submitted to the queue before 
+        # `q.task_done()` is called.  Hence the queue will not be
+        # empty if the current job should spawn new ones. 
 
 
 def main(lens="SIS",n=50,nproc=None,fn=None):
 
-    global num_processes
-
     if nproc is None: nproc = n
     print( f"Using {nproc} CPUs" )
-
     
     # The filename is generated from the number of amplitudes
     if fn is None: fn = str(n) + '.txt'
 
     start = time.time()
 
-    q = mp.JoinableQueue()
-    outq = mp.Queue()
+    q = mp.JoinableQueue()  # Input queue.  It is joinable to control completion.
+    outq = mp.Queue()       # Output queue
 
+    # Get and store the initial case m+s=1
     (a,b,x,y) = psiSIE()
+    dict = { (1,0) : a, (0,1) : b }
+
+    # Submit first round of jobs
     q.put( (2,0,a,x,y) )
     q.put( (1,1,a,x,y) )
     q.put( (0,2,b,x,y) )
 
-    dict = { (1,0) : a, (0,1) : b }
+    # Create a pool of workers to process the input queue.
+    pool = mp.Pool(nproc, mainworker,(q,outq,n,))
 
-    pool = mp.Pool(3, mainworker,(q,outq,))
-    q.join()
+    q.join()   # Block until the input jobs are completed
 
     print( "Time spent:", time.time() - start)
 
