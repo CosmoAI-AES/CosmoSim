7794cc4ec24b09317b033c4298ac2a9758962118
Hans Georg Schaathun <georg@schaathun.net>
2024-10-03 19:43:17 +0200

    moved amplitudes calculations to SampledLens and PsiFunctionLens.  Lens is abstract.

--- include/cosmosim/Lens.h
+++ include/cosmosim/Lens.h
@@ -14,9 +14,6 @@ using namespace SymEngine;
 
 class Lens {
 
-private:
-    std::array<std::array<LambdaRealDoubleVisitor, 202>, 201> alphas_l;
-    std::array<std::array<LambdaRealDoubleVisitor, 202>, 201> betas_l;
 
 
 protected:
@@ -25,8 +22,6 @@ protected:
 	   orientation=0 /* \phi */ ;
     std::string filename = "nosuchfile" ;
 
-    std::array<std::array<double, 202>, 201> alphas_val;
-    std::array<std::array<double, 202>, 201> betas_val;
     int nterms=20;
 
 public:
@@ -35,15 +30,15 @@ public:
     virtual void setRatio( double ) ;
 
 
-    virtual void initAlphasBetas();
-    virtual void calculateAlphaBeta( cv::Point2d xi );
+    virtual void initAlphasBetas() = 0 ;
+    virtual void calculateAlphaBeta( cv::Point2d xi ) = 0;
     void setFile(std::string) ;
     void setNterms(int) ;
 
-    double getAlpha( cv::Point2d xi, int m, int s ) ;
-    double getBeta( cv::Point2d xi, int m, int s ) ;
-    double getAlphaXi( int m, int s ) ;
-    double getBetaXi( int m, int s ) ;
+    virtual double getAlpha( cv::Point2d xi, int m, int s ) = 0 ;
+    virtual double getBeta( cv::Point2d xi, int m, int s ) = 0 ;
+    virtual double getAlphaXi( int m, int s ) = 0 ;
+    virtual double getBetaXi( int m, int s ) = 0 ;
     double getEinsteinR( ) const ;
 
     double getOrientation( ) const ;
@@ -59,11 +54,20 @@ public:
 };
 
 class SampledLens : public Lens {
+private:
+    std::array<std::array<double, 202>, 201> alphas_val;
+    std::array<std::array<double, 202>, 201> betas_val;
 protected:
     cv::Mat psi, psiX, psiY ;
 public:
+    virtual void initAlphasBetas();
     virtual void calculateAlphaBeta( cv::Point2d xi );
     virtual cv::Point2d getXi( cv::Point2d ) ;
+
+    virtual double getAlphaXi( int m, int s ) ;
+    virtual double getBetaXi( int m, int s ) ;
+    virtual double getAlpha( cv::Point2d xi, int m, int s ) ;
+    virtual double getBeta( cv::Point2d xi, int m, int s ) ;
 
     virtual double psiValue( double, double ) const ;
     virtual double psiXvalue( double, double ) const ;
@@ -75,8 +79,19 @@ public:
 } ;
 
 class PsiFunctionLens : public Lens {
+private:
+    std::array<std::array<LambdaRealDoubleVisitor, 202>, 201> alphas_l;
+    std::array<std::array<LambdaRealDoubleVisitor, 202>, 201> betas_l;
+    std::array<std::array<double, 202>, 201> alphas_val;
+    std::array<std::array<double, 202>, 201> betas_val;
 public:
+    virtual void initAlphasBetas();
+    virtual void calculateAlphaBeta( cv::Point2d xi );
 
+    virtual double getAlpha( cv::Point2d xi, int m, int s ) ;
+    virtual double getBeta( cv::Point2d xi, int m, int s ) ;
+    virtual double getAlphaXi( int m, int s ) ;
+    virtual double getBetaXi( int m, int s ) ;
 } ;
 
 class SampledPsiFunctionLens : public SampledLens {
