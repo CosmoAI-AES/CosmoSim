5876af63ef7f93392ffb224ff05dfa2a3c2c3711
Hans Georg Schaathun <georg@schaathun.net>
2024-10-17 08:02:31 +0200

    PointMass test runs.  The models do not match

--- Test/pointmass/pmtest.sh
+++ Test/pointmass/pmtest.sh
@@ -9,12 +9,12 @@ dir1=Exact
 dir2=Raytrace
 diffdir=diff
 
-mkdir -p $dir1 $dir2 $diffdir
+mkdir -p $dir1 $dir2 $diffdir montage
 
 fn=../spheres.csv
 
 python3 $pdir/datagen.py --config p --directory="$dir1" --csvfile $fn  
-python3 $pdir/datagen.py --lens PointMass --model Raytrace --directory="$dir2" --csvfile $fn  --actual --mask
+python3 $pdir/datagen.py --lens PM --model Raytrace --directory="$dir2" --csvfile $fn  --actual --mask
 python3 $pdir/compare.py --diff $diffdir $dir1 $dir2
 
 mkdir -p actual
@@ -23,5 +23,5 @@ mv "$dir2"/actual-* actual/
 for f in diff/*
 do
   ff=`basename $f`
-  $CONVERT \( actuals/actual-$ff "$dir1"/$ff +append \) \( "$dir2"/$ff diff-sampled-compare/$ff +append \) -append montage/$ff
+  convert \( actual/actual-$ff "$dir1"/$ff +append \) \( "$dir2"/$ff diff/$ff +append \) -append montage/$ff
 done
