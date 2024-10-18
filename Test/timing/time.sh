120b4d256b1b72fe4488370e6fc5e9aeef5e9ef0
Hans Georg Schaathun <georg@schaathun.net>
2024-10-18 06:50:44 +0200

    safe files copied from develop

--- Test/timing/time.sh
+++ Test/timing/time.sh
@@ -4,7 +4,7 @@
 
 opt=$*
 
-pdir=../../Python
+pdir=../../CosmoSimPy
 dir1=/tmp/SampledSIS
 dir2=/tmp/RouletteSIS
 diffdir=/tmp/diff
@@ -14,7 +14,7 @@ mkdir -p $dir1 $dir2 $diffdir
 fn=time.csv
 
 echo Sampled Model
-/usr/bin/time  python3 $pdir/datagen.py $opt -L ss --directory="$dir1" --csvfile $fn $* >/dev/null
-echo Regular Model
-/usr/bin/time python3 $pdir/datagen.py $opt -L sr --directory="$dir2" --csvfile $fn $* >/dev/null
+/usr/bin/time  python3 $pdir/datagen.py $opt --config ss --directory="$dir1" --csvfile $fn $* >/dev/null
+echo Regular Roulette Model
+/usr/bin/time python3 $pdir/datagen.py $opt --config fs --directory="$dir2" --csvfile $fn $* >/dev/null
 
