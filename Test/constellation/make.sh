1a3e5fd1deb6a4396809e76e3ae67a46ff8333cf
Hans Georg Schaathun <georg@schaathun.net>
2024-10-26 09:31:43 +0200

    Drafted new test

---
+++ Test/constellation/make.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+# Deprecated in favour of RegressionTest/cluster
+
+. ../../pythonenv/bin/activate
+
+( cd ../.. && cmake --build build ) || exit 1
+
+opt=$*
+
+pdir=../../CosmoSimPy/
+
+mkdir -p constellation
+
+python3 $pdir/datagen.py $opt --model Raytrace --csvfile constellation.csv --directory=onstellation --actual -R 
