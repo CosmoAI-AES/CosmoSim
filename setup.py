868bfb16f08a5af0b05c1bee7221da2af7bf5703
Hans Georg Schaathun <georg@schaathun.net>
2025-07-29 10:22:59 +0200

    Moved python module to src

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 setup(  # https://scikit-build.readthedocs.io/en/latest/usage.html#setup-options
     name="CosmoSim",
     version="2.5.3",
-    packages=find_packages("CosmoSimPy"),  # Include all packages in `./src`.
+    packages=find_packages("src"),  # Include all packages in `./src`.
     description="Simulator of Gravitational Lenses",
     author='Hans Georg Schaathun et al',
     license="MIT",
     python_requires=">=3.10",
-    package_dir={"": "CosmoSimPy"},  # The root for our python package is in `./src`.
+    package_dir={"": "src"},  # The root for our python package is in `./src`.
     install_requires=["sympy>=1.13", "numpy", "matplotlib", "opencv-python", "pandas"],  # Python Dependencies
     conan_requirements=["symengine/0.11.2", "opencv/4.11.0", "xz_utils/5.4.5", "zlib/1.2.13" ],  # C++ Dependencies
     cmake_minimum_required_version="3.23",
