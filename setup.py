d7596d1147a0d18b050b939ec00ec0b37a01d05f
Hans Georg Schaathun <georg@schaathun.net>
2025-07-29 19:38:00 +0200

    Updated pypi config from build branch

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import find_packages
 setup(  # https://scikit-build.readthedocs.io/en/latest/usage.html#setup-options
     name="CosmoSim",
     version="2.5.3",
-    packages=find_packages("CosmoSimPy"),  # Include all packages in `./src`.
+    packages=find_packages("src"),  # Include all packages in `./src`.
+    # packages=[ "CosmoSim", "CosmoSim/CosmoSimPy" ],
     description="Simulator of Gravitational Lenses",
     author='Hans Georg Schaathun et al',
     license="MIT",
     python_requires=">=3.10",
-    package_dir={"": "CosmoSimPy"},  # The root for our python package is in `./src`.
+    package_dir={"": "src"},  # The root for our python package is in `./src`.
     install_requires=["sympy>=1.13", "numpy", "matplotlib", "opencv-python", "pandas"],  # Python Dependencies
+    include_package_data=True,
     conan_requirements=["symengine/0.11.2", "opencv/4.11.0", "xz_utils/5.4.5", "zlib/1.2.13" ],  # C++ Dependencies
     cmake_minimum_required_version="3.23",
+    cmake_install_dir="src/",
+    package_data={ "CosmoSim" : [ "*.txt", "*.png", "*.so", "*.pyd" ] },
 )
 
