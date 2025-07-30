95414f957684fe862b08791047b90f8f5a6eb7e9
Hans Georg Schaathun <georg@schaathun.net>
2025-07-30 23:28:48 +0200

    Pruned redundant conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -13,8 +13,6 @@ class CosmoSimConan(ConanFile):
     def requirements(self):
         self.requires( "symengine/0.11.2" )
         self.requires( "opencv/4.11.0" )
-        self.requires( "xz_utils/5.4.5" )
-        self.requires( "zlib/1.2.13" )
 
     def layout(self):
         cmake_layout(self)
