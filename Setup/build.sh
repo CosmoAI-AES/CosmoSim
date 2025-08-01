56d1351713de3a0514efe7e1f0adc1d4eff67d1e
Hans Georg Schaathun <georg@schaathun.net>
2025-08-01 22:46:17 +0200

    Corrected exit code condition in build script.

--- Setup/build.sh
+++ Setup/build.sh
@@ -19,5 +19,5 @@ conan install . --output-folder=build --build=missing
   cmake .. -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release && \
   cmake --build . && \
   cmake --install . --prefix=../src
-) || \
+) && \
 python CosmoSimPy/datagen.py
