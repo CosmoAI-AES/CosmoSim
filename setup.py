945558489c5d2bbea6943a4e8fb91a85334bd20f
Hans Georg Schaathun <georg@schaathun.net>
2025-08-03 11:47:06 +0200

    Reverted cmake to minimum verfsion 3.15.  New installs.  Try building on manylinux 2.34

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(  # https://scikit-build.readthedocs.io/en/latest/usage.html#setup-options
     package_dir={"": "src"},  # The root for our python package is in `./src`.
     install_requires=["sympy>=1.13", "numpy", "matplotlib", "opencv-python", "pandas"],  # Python Dependencies
     include_package_data=True,
-    conan_requirements=[ "symengine/0.11.2", "opencv/4.12.0" ],  # C++ Dependencies
+    conan_requirements=[ "symengine/0.14.0", "opencv/4.12.0" ],  # C++ Dependencies
     cmake_minimum_required_version="3.23",
     cmake_install_dir="src/",
     package_data={ "CosmoSim" : [ "*.txt", "*.png", "*.so", "*.pyd" ] },
