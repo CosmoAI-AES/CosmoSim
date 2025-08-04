2fa65495b0e5da73c6f595021063f022a94f07b4
Hans Georg Schaathun <georg@schaathun.net>
2025-08-04 10:56:42 +0200

    Fixed roulette gen regression tests

--- RegressionTest/roulettegen/makeimages.sh
+++ RegressionTest/roulettegen/makeimages.sh
@@ -2,4 +2,4 @@
 dir=`date "+%Y%m%d"`
 mkdir -p $dir
 . ../bin/config.sh
-roulettegen.py -n 10 -Z 600 --csvfile roulette.csv -D $dir 
+python -m CosmoSim.roulettegen -n 10 -Z 600 --csvfile roulette.csv -D $dir 
