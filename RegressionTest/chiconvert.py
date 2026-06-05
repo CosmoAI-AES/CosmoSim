0ffbab565a3029d8fcf6827023f60169d4cd0f55
Hans Georg Schaathun <georg@schaathun.net>
2026-06-05 07:38:54 +0200

    Converted test data to angular model

---
+++ RegressionTest/chiconvert.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser( prog='chiconvert')
+parser.add_argument('infile')
+parser.add_argument('outfile')
+args = parser.parse_args()
+
+df = pd.read_csv( args.infile )
+
+
+cols = [ "x", "y", "sigma", "sigma2" ]
+for k in cols:
+   df[k] = df[k]* df["chi"]/100
+
+df.to_csv( args.outfile, index=False)
+
